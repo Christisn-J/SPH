851320d62db71a7f1aa4ed2bad768bf1225996df
Christisn-J <christian.jetter@student.uni-tuebingen.de>
2023-01-11 16:07:28 +0100

    initial

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
-#include <cxxopts.hpp> // parser
 
+// #include <cxxopts.hpp> // parser
+
+#include "initial.h"
 #include "Particles.h"
 
 
 int main(){
+    load("...")
+    Particles sampel = Particles
+    init(sampel)
 
+    while t < T_MAX{
+        
+    }
 
     std::cout <<"Hello Wolrd\n";
     return 1;
